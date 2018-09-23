01a66b2bdc9cc1a43ca8aeea6d7e5a3916ac5e01
cromat <brabec.tomislav@gmail.com>
2018-09-24 00:05:32 +0200

    mint changes

--- apps.sh
+++ apps.sh
@@ -1,15 +1,11 @@
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo add-apt-repository -y ppa:me-davidsansome/clementine
-sudo add-apt-repository -y ppa:screenlets/ppa
 sudo add-apt-repository -y ppa:linuxthebest.net/sopcast
+sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update && \
 sudo apt-get --assume-yes install \
 p7zip-full \
-clementine \
 zip \
 net-tools \
 openssl \
-nano \
 iputils-ping \
 vim \
 wget \
@@ -24,20 +20,13 @@ man \
 sysstat \
 dstat \
 openssh-client \
-openssh-server \
 software-properties-common \
 psmisc \
-pg-activity \
 bash-completion \
 whois \
 git \
-phppgadmin \
-sublime-text-installer \
-terminator \
-guake \
 clipit \
 filezilla \
-chromium-browser \
 gksu \
 aptitude \
 alacarte \
@@ -46,10 +35,7 @@ screen \
 unzip \
 strace \
 shutter \
-screenlets \
-screenlets-pack-all \
 kazam \
-vlc \
 unetbootin \
 hostapd \
 gdebi-core \
@@ -59,8 +45,21 @@ python-m2crypto \
 python-appindicator \
 libx11-dev \
 sopcast-player \
-pavucontrol \
-ncdu
+ncdu \
+ubuntu-make \
+virtualbox
+
+# VS Code
+yes | umake ide visual-studio-code --accept-license
+echo -e "[Desktop Entry]\nVersion=1.0\nType=Application\nName=Visual Studio Code\nIcon=$HOME/code/resources/app/resources/linux/code.png\nExec=\"$HOME/.local/share/umake/bin/visual-studio-code\" %f\nCategories=Development;IDE;\nTerminal=false" > $HOME/.local/share/applications/visual-studio-code.desktop
+
+# Opera
+sudo wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
+sudo apt update
+sudo apt install opera-stable
+sudo sed -i "s/Exec=opera %U/Exec=opera %U --start-maximized/g" /usr/share/applications/opera.desktop
+sudo ln -sf /usr/lib/chromium-browser/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so
 
 # Remove sopcast repository because it will not be updated
 sudo add-apt-repository --remove -y ppa:linuxthebest.net/sopcast
@@ -74,12 +73,9 @@ sudo cp vlsub-master/vlsub.lua /usr/lib/vlc/lua/extensions/
 cp open-as-administrator ~/.local/share/nautilus/scripts
 chmod 0755 ~/.local/share/nautilus/scripts/open-as-administrator
 
-# Bash it framework
+# Bash-it framework
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
-
-# Xampp dashboard desktop
-cp xampp.desktop ~/.local/share/applications/
 
 # Restricted extras (multimedia plugins)
 sudo apt-get -qq -y install ubuntu-restricted-extras
@@ -90,4 +86,3 @@ sudo gdebi --n acestream_ubuntu_64/acestream-engine_3.0.5.1-0.2_amd64.deb
 sudo gdebi --n acestream_ubuntu_64/acestream-player-data_3.0.2-1.1_amd64.deb
 sudo gdebi --n acestream_ubuntu_64/acestream-player-compat_3.0.2-1.1_amd64.deb
 sudo gdebi --n acestream_ubuntu_64/acestream-player_3.0.2-1.1_amd64.deb
-# TODO add acestream icon
